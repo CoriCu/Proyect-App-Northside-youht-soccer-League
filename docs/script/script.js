e53384aa180ca07a3b14bd6a9b92bf7e4f5c036e
CoriCu <98062467+CoriCu@users.noreply.github.com>
2022-03-04 00:49:45 -0300

    Add files via upload

---
+++ docs/script/script.js
@@ -0,0 +1,194 @@
+var meses = [{
+    "month_name":"september",
+    "teams": [
+        {
+            "vsName":"U1 and U4",
+            "location":"AJ Katzenmaier",
+            "maps":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4281.467703332423!2d-87.62962089391536!3d41.8988457523852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f69da7%3A0x15e198c063fc787c!2sAJ%20Katzenmaier%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001955054!5m2!1ses-419!2sar",
+            "times":"9:30a.m.",
+            "day":"01"
+        },
+        {
+            "vsName":"U3 and U2",
+            "location":"Greenbay",
+            "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d22766.154292832573!2d-88.055104!3d44.499394!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8802fa546caa53a7%3A0x8e2b8e336a5f95f8!2s820%20Armed%20Forces%20Dr%2C%20Green%20Bay%2C%20WI%2054304%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1646358151740!5m2!1ses-419!2sar" ,
+            "times":"1:00 p.m.",
+            "day":"01"
+        },
+        {
+            "vsName":"U5 and U6",
+            "location":"Howard A Yeager",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23602.20545993103!2d-87.8713339604492!3d42.31531840000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccaa32b2b93%3A0xc2120a8237aaa4e4!2s1811%20Morrow%20Ave%2C%20North%20Chicago%2C%20IL%2060064%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1646358466883!5m2!1ses-419!2sar",
+            "times":"9:30a.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U6 and U1",
+            "location":"Marjorie P Hart",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23748.589278423817!2d-87.646262!3d41.923523!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3102866e98b%3A0x6f95a4b19f6f6a1!2sN%20Orchard%20St%2C%20Chicago%2C%20IL%2060614%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1646358633133!5m2!1ses!2sar",
+            "times":"1:00 p.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U2 and U4",
+            "location":"North",
+            "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26738.56398566679!2d-96.81986!3d33.100657!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c3c96acb30463%3A0x8b43bf85a687e29e!2s2601%20Avenue%20of%20the%20Stars%2C%20Frisco%2C%20TX%2075034%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1646358896124!5m2!1ses!2sar",
+            "times":"9:30a.m.",
+            "day":"15"
+        },
+        {
+            "vsName":"U3 and U5	",
+            "location":"AJ Katzenmaier",
+    	    "maps": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4281.467703332423!2d-87.62962089391536!3d41.8988457523852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f69da7%3A0x15e198c063fc787c!2sAJ%20Katzenmaier%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001955054!5m2!1ses-419!2sar",
+            "times":"1:00 p.m.",
+            "day":"15"
+        },
+        {
+            "vsName":"U1 and U3",
+            "location":"South",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26738.56398566679!2d-96.81986!3d33.100657!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c3c96acb30463%3A0x8b43bf85a687e29e!2s2601%20Avenue%20of%20the%20Stars%2C%20Frisco%2C%20TX%2075034%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1646358759449!5m2!1ses!2sar",
+            "times":"9:30a.m.",
+            "day":"22"
+        },
+        {
+            "vsName":"U2 and U6",
+            "location":"Howard A Yeager",
+            "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23602.20560994482!2d-87.853824!3d42.315318!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccaa32b2b93%3A0xc2120a8237aaa4e4!2s1811%20Morrow%20Ave%2C%20North%20Chicago%2C%20IL%2060064%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1646358994157!5m2!1ses-419!2sar",
+            "times":"1:00 p.m.",
+            "day":"22"
+        },
+        {
+            "vsName":"U4 and U5",
+            "location":"Greenbay",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d22766.154292832573!2d-88.055104!3d44.499394!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8802fa546caa53a7%3A0x8e2b8e336a5f95f8!2s820%20Armed%20Forces%20Dr%2C%20Green%20Bay%2C%20WI%2054304%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1646359051109!5m2!1ses-419!2sar",
+            "times":"9:30a.m.",
+            "day":"29"
+        },
+    ]
+},
+{
+    "month_name":"october",
+    "teams": [
+        {
+            "vsName":"U2 and U5",
+            "location":"Marjorie P Hart",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23748.589278423817!2d-87.646262!3d41.923523!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3102866e98b%3A0x6f95a4b19f6f6a1!2sN%20Orchard%20St%2C%20Chicago%2C%20IL%2060614%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1646358633133!5m2!1ses!2sar",
+            "times":"9:30a.m.",
+            "day":"06"
+        },
+        {
+            "vsName":"U1 and U6",
+            "location":"South",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26738.56398566679!2d-96.81986!3d33.100657!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c3c96acb30463%3A0x8b43bf85a687e29e!2s2601%20Avenue%20of%20the%20Stars%2C%20Frisco%2C%20TX%2075034%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1646358759449!5m2!1ses!2sar",
+            "times":"1:00 p.m.",
+            "day":"06"
+        },
+        {
+            "vsName":"U3 and U4",
+            "location":"Howard A Yeager",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23602.20560994482!2d-87.853824!3d42.315318!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccaa32b2b93%3A0xc2120a8237aaa4e4!2s1811%20Morrow%20Ave%2C%20North%20Chicago%2C%20IL%2060064%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1646358994157!5m2!1ses-419!2sar",
+            "times":"9:30a.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U5 and U1",
+            "location":"Greenbay",
+	    "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d22766.154292832573!2d-88.055104!3d44.499394!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8802fa546caa53a7%3A0x8e2b8e336a5f95f8!2s820%20Armed%20Forces%20Dr%2C%20Green%20Bay%2C%20WI%2054304%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1646359051109!5m2!1ses-419!2sar",
+            "times":"1:00 p.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U6 and U3",
+            "location":"North",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26738.56398566679!2d-96.81986!3d33.100657!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c3c96acb30463%3A0x8b43bf85a687e29e!2s2601%20Avenue%20of%20the%20Stars%2C%20Frisco%2C%20TX%2075034%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1646358896124!5m2!1ses!2sar",
+            "times":"9:30a.m.",
+            "day":"20	"
+        },
+        {
+            "vsName":"U2 and U4",
+            "location":"Marjorie P Hart",
+            "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23748.589278423817!2d-87.646262!3d41.923523!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3102866e98b%3A0x6f95a4b19f6f6a1!2sN%20Orchard%20St%2C%20Chicago%2C%20IL%2060614%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1646358633133!5m2!1ses!2sar",
+            "times":"1:00 p.m.",
+            "day":"20	"
+        },
+        {
+            "vsName":"U3 and U1",
+            "location":"AJ Katzenmaier",
+            "maps":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4281.467703332423!2d-87.62962089391536!3d41.8988457523852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f69da7%3A0x15e198c063fc787c!2sAJ%20Katzenmaier%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001955054!5m2!1ses-419!2sar",
+            "times":"9:30a.m.",
+            "day":"27"
+        },
+        {
+            "vsName":"U5 and U6",
+            "location":"Howard A Yeager",
+	        "maps":"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d23602.20560994482!2d-87.853824!3d42.315318!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccaa32b2b93%3A0xc2120a8237aaa4e4!2s1811%20Morrow%20Ave%2C%20North%20Chicago%2C%20IL%2060064%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1646358994157!5m2!1ses-419!2sar",
+            "times":"1:00 p.m.",
+            "day":"27"
+        }
+    ]
+}
+]
+
+
+
+let tablat = document.getElementById('info');
+let tablac = document.createElement('tbody');
+
+
+meses[0].teams.forEach(p=>{
+    let fila = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerText = p.day;
+    fila.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = p.vsName;
+    fila.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = p.location;
+    td.innerHTML = `<iframe src="${p.maps}"> </iframe>` ;
+    fila.appendChild(td);
+    
+    
+    td = document.createElement('td');
+    td.innerText = p.times;
+    fila.appendChild(td);
+
+    tablac.appendChild(fila)
+});
+
+tablat.appendChild(tablac);
+
+let tablaa = document.getElementById('info2');
+let tablacu = document.createElement('tbody');
+
+meses[1].teams.forEach(p=>{
+    let fila = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerText = p.day;
+    fila.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = p.vsName;
+    fila.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = p.location;
+    td.innerHTML = `<iframe src="${p.maps}"> </iframe>` ;
+    fila.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = p.times;
+    fila.appendChild(td);
+
+
+    tablacu.appendChild(fila)
+})
+
+tablaa.appendChild(tablacu)
+
+
+
